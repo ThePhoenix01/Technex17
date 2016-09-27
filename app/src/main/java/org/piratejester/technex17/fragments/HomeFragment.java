b6d075c7215804f05754489ae41968f89d1bdacd
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2016-09-27 20:31:18 +0530

    Home Screen Finalised and Scrolling Bug Fixed

--- app/src/main/java/org/piratejester/technex17/fragments/HomeFragment.java
+++ app/src/main/java/org/piratejester/technex17/fragments/HomeFragment.java
@@ -1,24 +1,19 @@
 package org.piratejester.technex17.fragments;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import org.piratejester.technex17.R;
-import org.piratejester.technex17.adapters.NavigHomeAdapter;
-import org.piratejester.technex17.adapters.RecyclerItemClickListener;
 
 /**
  * Created by root on 18/9/16.
@@ -78,60 +73,25 @@ public class HomeFragment extends Fragment {
     }
 
     private void setupNavigRecyclerView() {
-        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.navig_recycler);
+        FrameLayout cv1;
+        CardView cv2,cv3,cv4,cv5,cv6,cv7,cv8,cv9;
+        cv1=(FrameLayout)rootView.findViewById(R.id.cv1);
+        cv2=(CardView)rootView.findViewById(R.id.cv2);
+        cv3=(CardView)rootView.findViewById(R.id.cv3);
+        cv4=(CardView)rootView.findViewById(R.id.cv4);
+        cv5=(CardView)rootView.findViewById(R.id.cv5);
+        cv6=(CardView)rootView.findViewById(R.id.cv6);
+        cv7=(CardView)rootView.findViewById(R.id.cv7);
+        cv8=(CardView)rootView.findViewById(R.id.cv8);
+        cv9=(CardView)rootView.findViewById(R.id.cv9);
 
-        String[] titles = {"Events",
-                "Guest Lectures",
-                "Workshops",
-                "StartUp Fair",
-                "Video Wall", "Startup Fair",
-                "Guest Lectures",
-                "Workshops"
-        };
-        String[] subtitles = {"Aim for Glory",
-                "Soak in the Wisdom",
-                "Learn by Fun",
-                "An Awesome Beginning",
-                "Lets get graphic",
-                "Ringa Ringa Roses",
-                "Pocket Full of Poises",
-                "Atishu Atishu",
-                "We all fall down"
-        };
-        Bitmap[] icons = {BitmapFactory.decodeResource(getResources(), R.drawable.trophy_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.lecture_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.workshop_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.rocket_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.video_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.news_2_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.user_m_white),
-                BitmapFactory.decodeResource(getResources(), R.drawable.home_white)
-        };
-        int[] colors = {getResources().getColor(R.color.md_red_trans),
-                getResources().getColor(R.color.md_green_trans),
-                getResources().getColor(R.color.md_deep_orange_trans),
-                getResources().getColor(R.color.md_indigo_trans),
-                getResources().getColor(R.color.md_teal_trans),
-                getResources().getColor(R.color.md_pink_trans),
-                getResources().getColor(R.color.md_purple_trans),
-                getResources().getColor(R.color.md_blue_trans)
-        };
-
-
-        RecyclerView.LayoutManager mLayoutManager;
-        mLayoutManager = new GridLayoutManager(getActivity(),2);
-        recyclerView.setLayoutManager(mLayoutManager);
-        NavigHomeAdapter navigHomeAdapter = new NavigHomeAdapter(titles, subtitles, icons, colors);
-        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        Log.d("Clicked", "" + position);
-                        ViewPager viewPager = (ViewPager)getActivity().findViewById(R.id.container);
-                        viewPager.setCurrentItem(2);
-                    }
-                })
-        );
-        recyclerView.setAdapter(navigHomeAdapter);
+        cv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewPager viewPager = (ViewPager)getActivity().findViewById(R.id.container);
+                viewPager.setCurrentItem(2);
+            }
+        });
 
     }
 
